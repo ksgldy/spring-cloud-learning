d1ef19d711c7e34b62704e66efe1413f822c2d5a
qidian360 <idea360@foxmail.com>
2020-04-19 02:31:35 +0800

    gson adater

--- idc-wechat/src/main/java/cn/idea360/idcwechat/xml/Test.java
+++ idc-wechat/src/main/java/cn/idea360/idcwechat/xml/Test.java
@@ -4,14 +4,28 @@ import com.thoughtworks.xstream.XStream;
 
 public class Test {
 
-    public static void main(String[] args) {
-        String xml = "<xml>\n" +
-                "  <ToUserName><![CDATA[toUser]]></ToUserName>\n" +
-                "</xml>";
+//    public static void main(String[] args) {
+//        String xml = "<xml>\n" +
+//                "  <ToUserName><![CDATA[toUser]]></ToUserName>\n" +
+//                "</xml>";
+//
+//        XStream stream = new XStream();
+//        stream.processAnnotations(Tm.class);
+//        Tm testEntity = (Tm) stream.fromXML(xml);
+//        System.out.println(testEntity);
+//    }
 
-        XStream stream = new XStream();
-        stream.processAnnotations(new Class[]{Tm.class});
-        Tm testEntity = (Tm) stream.fromXML(xml);
-        System.out.println(testEntity);
+
+    public static void main(String[] args) {
+        XStream xStream = new XStream();
+        xStream.alias("User", User.class);
+        String xml = "<User>\n" +
+                "  <userName>lanweihong</userName>\n" +
+                "  <email>lwhhhp@gmail.com</email>\n" +
+                "</User>";
+        //转对象
+        User user = (User)xStream.fromXML(xml);
+        System.out.println(user.toString());
+
     }
 }
---
+++ idc-wechat/src/main/java/cn/idea360/idcwechat/xml/User.java
@@ -0,0 +1,34 @@
+package cn.idea360.idcwechat.xml;
+
+public class User {
+    private String userName;
+
+    private String email;
+
+    public User() {}
+
+    public User(String userName, String email) {
+        this.userName = userName;
+        this.email = email;
+    }
+
+    public String toString() {
+        return "User:{userName=" + this.userName + ",email=" + this.email + "}";
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
